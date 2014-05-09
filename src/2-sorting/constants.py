e0c0e02f1992f2c2d48633fbc3af1a4f59016434
Marina Wahl <mari.wahl9@gmail.com>
2014-05-08 20:49:48 -0400

    more data

--- src/2-sorting/constants.py
+++ src/2-sorting/constants.py
@@ -10,7 +10,5 @@ __version__ = "2.0"
 __maintainer__ = "Mari Wahl"
 __email__ = "marina.w4hl@gmail.com"
 
-
-SUBFOLDERS = ['atlas', 'auto', 'carbon',  'citation', 'ground', 'location', 'products', 'p2p', 'road','wiki','yeast']
-#'cellular','collaboration','communication','location', 'metabolic','social','webgraphs'
+SUBFOLDERS = ['atlas', 'auto', 'carbon',  'citation', 'ground', 'location', 'products', 'p2p', 'road','wiki','yeast','cellular','collaboration','communication', 'metabolic','social','webgraphs']
 FEATURES   = ['Size', 'Order', 'Assortativity', 'Transitivity', 'Degree', 'Coreness', 'Number_Triangles', 'Number_Cliques', 'Clique_number', 'Clustering', 'Edge_connectivity', 'Eccentricity', 'Diameter', 'Betweeness', 'Density', 'Radius', 'Square_clustering', 'Communicability', 'Pagerank', 'Centrality']
